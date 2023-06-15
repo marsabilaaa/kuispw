272726023fd8d41f37a0b9c3703f2d346dab2c6c
marsabilaaa <marsabilaaa@gmail.com>
2023-06-15 23:19:10 +0700

    nomor soal

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,21 +7,21 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+    //Kuis nomor 2a
     public function available()
     {
         $products = Product::where('stock', '>', 0)->get();
 
         return view('products.available', compact('products'));
     }
-
+    //Kuis nomor 2b
     public function unavailable()
     {
         $products = Product::where('stock', 0)->get();
 
         return view('products.unavailable', compact('products'));
     }
-
+    //kuis nomor 1a
     public function create()
     {
         return view('products.create');
@@ -39,21 +39,21 @@ class ProductController extends Controller
 
         return redirect()->route('products.index')->with('success', 'Product created successfully.');
     }
-
+//kuis nomor 1b
     public function index()
     {
         $products = Product::all();
 
         return view('products.index', compact('products'));
     }
-
+//kuis nomor 1c
     public function show($id)
     {
         $product = Product::findOrFail($id);
 
         return view('products.show', compact('product'), );
     }
-
+//kuis nomor 1d
     public function edit($id)
     {
         $product = Product::findOrFail($id);
@@ -74,7 +74,7 @@ class ProductController extends Controller
 
         return redirect()->route('products.index');
     }
-
+//kuis nomor 1e
     public function destroy($id)
     {
         $product = Product::findOrFail($id);
@@ -82,7 +82,7 @@ class ProductController extends Controller
 
         return redirect()->route('products.index')->with('success', 'Product deleted successfully.');
     }
-
+//kuis nomor 2c
     public function editStock($id)
     {
         $product = Product::findOrFail($id);
